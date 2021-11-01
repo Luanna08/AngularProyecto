31f3c0cb3d7f5f074324749eca0b648c60f68e86
LUANNA FABIOLA GONZALEZ ARTAVIA <lfgonzalez@est.utn.ac.cr>
2021-11-01 13:57:08 -0600

    proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:'',component: HomeComponent}, {path:'login',component: LoginComponent},
+ {path:'register',component: RegisterComponent}, {path:'about',component: AboutComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './pages/nav/nav.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
